8bdc02079a37b756e7170d7cfd6e524ccfb72023
njjones8 <njjones8@asu.edu>
2022-07-20 16:42:20 -0700

    changed connection to real database

--- prototype/dbconnect.py
+++ prototype/dbconnect.py
@@ -14,14 +14,14 @@ nltk.download('punkt')
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize
 
-# database host is database address (localhost if on local computer)
-# database name is name of database
-db_host = "ec2-54-165-90-230.compute-1.amazonaws.com"   # address of database
-db_name = "d6hp3i25m6gslc"      # name of database
+# Database host is database address (localhost if on local computer)
+# Database name is name of database
+db_host = "ec2-18-215-96-22.compute-1.amazonaws.com"   # address of database
+db_name = "db926f75ofgha2"      # name of database
 
-# data base username/pass
-db_user = "uqqcowyaruajrk"        # login name
-db_pass = "e8828f90c6df41a82eac46bcb552a9cdf32a5b109db1d72ec7cb9ad988030475"      # login password
+# Data base username/pass
+db_user = "glkawdwwyajxsd"        # login name
+db_pass = "ab13a8d4d623d059a6c6fa355da18839af9ce573edc56ae8ab09ba0b99b70a72"      # login password
 db_port = 5432
 
 # can use a user account only with permissions to read from database to make sure
@@ -33,14 +33,14 @@ connect = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=
 cursor = connect.cursor()
 
 cursor.execute("""
-set transaction read write;
 
-CREATE TABLE tenantapp(
-  
-  appid numeric PRIMARY KEY NOT NULL
-
-);
+SELECT * FROM homeownerapp;
 
 """)
+
+temp = cursor.fetchall()
+for x in temp:
+    print("\n\n")
+    print(x)
 
 connect.close()
