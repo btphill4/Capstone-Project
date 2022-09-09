8ad90f0dc92b6f42b5f6e3208ccdf0d9aaca5f03
Brandon <btphill4@asu.edu>
2022-09-09 11:21:29 -0700

    Basic layout implemented

---
+++ methods.py
@@ -0,0 +1,26 @@
+# File to hold our methods to be called in main
+
+# Install for geopy -> pip install geopy
+# Install for pandas -> pip install pandas
+import pandas 
+import numpy
+from geopy import *
+
+# connect to database method
+
+
+
+# filter job type i.e. Gardening, etc method
+
+
+
+# filter Day and Time method
+
+
+
+# calculate and filter distance method
+
+
+
+# output?
+
