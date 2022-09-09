904c44e4a125bcabac4406ff1ab2aed5ccd2ef41
Brandon <btphill4@asu.edu>
2022-09-09 11:13:19 -0700

    Migrate Old Project and initialize new project

---
+++ Methods.py
@@ -0,0 +1,21 @@
+# File to hold our methods to be called in main
+
+# connect to database
+
+
+
+# filter job type i.e. Gardening, etc
+
+
+
+
+# filter Day and Time 
+
+
+
+# filter distance
+
+
+
+# output?
+
